e97472cf53e480ba76b6d69345e14b363f68c8e8
Lanti <lanti_0109@yahoo.co.id>
2023-02-16 21:20:03 +0700

    fix: login page without nav bar (still problem)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'PELITA';
   sideNavStatus: boolean = false;
 
-  isLoginPage = true;
+  isLoginPage!: boolean;
+
+  ngOnInit(): void {
+    this.isLoginPage = true;
+    console.log(this.isLoginPage);
+    
+  }
+
+  isGoToLoginPage(e: boolean) {
+    this.isLoginPage = e;
+  }
 }
