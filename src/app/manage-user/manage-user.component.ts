9cb58596e10c31272fd9dfb6267c73791f62584e
Lanti <lanti_0109@yahoo.co.id>
2023-02-18 23:21:05 +0700

    condition: add more item on manage user table

--- src/app/manage-user/manage-user.component.ts
+++ src/app/manage-user/manage-user.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./manage-user.component.scss']
 })
 export class ManageUserComponent {
+  idx = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 }
