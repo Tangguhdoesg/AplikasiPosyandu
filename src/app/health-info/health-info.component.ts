a01a1564f34b659adcc3237774e997c36e735589
lanti <lanti_0109@yahoo.co.id>
2023-04-19 16:33:52 +0700

    fix: add loading & to on info kesehatan balita

--- src/app/health-info/health-info.component.ts
+++ src/app/health-info/health-info.component.ts
@@ -14,7 +14,9 @@ export class HealthInfoComponent {
   graphData: healthInfoGraph[] = [];
   graphDataLen: number = 0;
 
-  optionx: any;
+  isLoading: boolean = false;
+  isError: boolean = false;
+
   optionWeight: any[] = [];
   optionHeight: any[] = [];
 
@@ -30,90 +32,11 @@ export class HealthInfoComponent {
     this.optionWeight = [];
     this.optionHeight = [];
     this.getAllUserGraph();
-
-
-    this.optionx = {
-      title: {
-        text: 'Data'
-      },
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'cross',
-          label: {
-            backgroundColor: '#6a7985'
-          }
-        }
-      },
-      legend: {
-        data: ['X-1', 'X-2', 'X-3', 'X-4', 'X-5']
-      }, 
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-      },
-      xAxis:  [
-        {
-          type: 'category',
-          boundaryGap: false,
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-          // splitLine: {
-          //   show: false
-          // }
-        }
-      ],
-      yAxis: {
-        type: 'value',
-        // splitLine: {
-        //   show: false
-        // }
-      },
-      series: [
-        {
-          name: 'X-1',
-          type: 'line',
-          // stack: 'counts',
-          color: 'red',
-          data: [120, 132, 101, 134, 90, 230, 210]
-        },
-        {
-          name: 'X-2',
-          type: 'line',
-          // stack: 'counts',
-          data: [220, 182, 191, 234, 290, 330, 310]
-        },
-        {
-          name: 'X-3',
-          type: 'line',
-          // stack: 'counts',
-          data: [150, 232, 201, 154, 190, 330, 410]
-        },
-        {
-          name: 'X-4',
-          type: 'line',
-          // stack: 'counts',
-          data: [320, 332, 301, 334, 390, 330, 320]
-        },
-        {
-          name: 'X-5',
-          type: 'line',
-          // stack: 'counts',
-          label: {
-            show: true,
-            position: 'top',
-          },
-          data: [820, 932, 0, 934, 1290, 1330, 1320]
-        }
-      ]
-    };
-
   }
 
   getAllUserGraph() {
-    // this.isLoading = true;
-    // this.isError = false;
+    this.isLoading = true;
+    this.isError = false;
     this.service.getAllUserGraph(sessionStorage.getItem('id'))
       .pipe(takeUntil(this.destroySubject$))
       .subscribe(datas => {
@@ -127,10 +50,10 @@ export class HealthInfoComponent {
             this.setHeigthGraph(gData);
           })
         }
-        // this.isLoading = false;
+        this.isLoading = false;
       }, err => {
-        // this.isError = true;
-        // this.isLoading = false;
+        this.isError = true;
+        this.isLoading = false;
       })
   }
 
